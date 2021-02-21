48eba297f3292ecf737ae7078cd5830adfc1b19e
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-02-21 19:26:31 +0300

    starter

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,30 +1,11 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Header from '@components/Header';
-import routesConfig from '@routes/routesConfig';
-import { REPO_NAME } from '@constants/repo';
-
 import styles from './App.module.css';
 
+import { getApiResource } from '../../utils/network';
+
 const App = () => {
-    return (
-        <>
-            <BrowserRouter basename={`/${REPO_NAME}/`}>
-                <div className={styles.wrapper}>
-                    <Header />
-                    <Switch>
-                        {routesConfig.map((route, index) => (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                exact={route.exact}
-                                component={route.component}
-                            />
-                        ))}
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        </>
-    )
+  return (
+    <h1 className={styles.header}>Hello</h1>
+  )
 }
 
 export default App;
