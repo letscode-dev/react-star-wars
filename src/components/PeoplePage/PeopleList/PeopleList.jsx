d9e4522ec8f958fc5d6c2ea6b09f0c6ebad0a52e
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-03-13 21:29:52 +0300

    starter

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -1,23 +1,24 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import styles from './PeopleList.module.css';
 
-const PeopleList = ({ people }) =>(
-    <ul className={styles.list__container}>
-        {people.map(({ id, name, img }) =>
-            <li className={styles.list__item} key={id}>
-                <Link to={`/people/${id}`}>
-                    <img className={styles.person__photo} src={img} alt={name} />
-                    <p>{name}</p>
-                </Link>
-            </li>
-        )}
-    </ul>
-)
+const PeopleList = ({ people }) => {
+    return (
+        <ul className={styles.list__container}>
+            {people.map(({ id, name, img }) => 
+                <li className={styles.list__item} key={id}>
+                    <a href="#">
+                        <img className={styles.person__photo} src={img} alt={name} />
+                        <p>{name}</p>
+                    </a>
+                </li>
+            )}
+        </ul> 
+    )
+}
 
 PeopleList.propTypes = {
-    people: PropTypes.array,
+    people: PropTypes.array
 }
 
 export default PeopleList;
