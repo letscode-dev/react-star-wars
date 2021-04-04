32d706b828cca0ecfe33623f3ebaae7cb5a6f287
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-04-04 11:47:11 +0300

    starter

--- src/hooks/useQueryParams.js
+++ src/hooks/useQueryParams.js
@@ -1,6 +1,5 @@
 import { useLocation } from 'react-router-dom';
 
-// Синтаксический анализ строки запроса
 export const useQueryParams = () => {
     return new URLSearchParams(useLocation().search);
 }
